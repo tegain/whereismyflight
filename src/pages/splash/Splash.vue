4341b69b99b2a9e6498ed563c45ec6140027d5a9
Thomas <thomas.egain@gmail.com>
2017-11-18 22:25:56 +0100

    Add Splash button css

--- src/pages/splash/Splash.vue
+++ src/pages/splash/Splash.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="app-Page app-Page__splash">
-    <p v-if="loading">
-      Chargement...
-    </p>
-    <p>
-      <router-link :to="{ name: 'Home' }" tag="button">Rechercher un vol</router-link>
-    </p>
+  <div class="app-Page app-Content__splash">
+    <transition name="loadSplash">
+      <div v-if="loading">
+        Chargement...
+      </div>
+      <div v-else>
+        <router-link :to="{ name: 'Home' }" tag="button" class="app-Btn">Rechercher un vol</router-link>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -22,3 +24,26 @@
     }
   }
 </script>
+
+<style lang="scss">
+  .app-Btn {
+    border: none;
+    cursor: pointer;
+    padding: .75rem 2.5rem;
+    border-radius: 2rem;
+    background: #ff00cc;
+    background: linear-gradient(to top, #773c99, #ff00cc);
+    background-size: 125% 100%;
+    background-position: 0 0;
+    color: #fff;
+    font-size: 1rem;
+    letter-spacing: .1rem;
+    font-family: 'Merriweather', Helvetica, Arial, sans-serif;
+    box-shadow: 0 1rem 2rem -.5rem rgba(#ff00cc, .25);
+    transition-duration: .4s;
+
+    &:hover {
+      background-position: 95% 0;
+    }
+  }
+</style>
